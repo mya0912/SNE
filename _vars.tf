b3297dfc5120a575a65ac4f6a3673b554dc694cb
Mike Alipui <myalipui@gmail.com>
2018-12-07 16:45:21 -0500

    terraform config file creating an ec2 instance and variables file

---
+++ _vars.tf
@@ -0,0 +1,9 @@
+#tf runtime variables specified in this file
+
+variable "region" {
+    type = "string"
+    default = "us-east-1"
+    description = "Environment Region"
+  }
+  
+
---
+++ main.tf
@@ -0,0 +1,32 @@
+#initialize terraform and specify backend
+terraform{
+  backend "s3"{}
+}
+#specify provider and region
+provider "aws" {
+    region = "${var.region}"
+}
+
+#this section is to networking resources - VPC, Security Groups, ELB, etc.
+
+/*this section is to define compute Resources
+#query ami id for ec2 instance creation*/
+data "aws_ami" "test_iis_server" {
+
+  filter{
+    name            = "name"
+    values          = ["Windows_Server-2016-English-Nano*"]
+  }
+
+  most_recent       = true
+}
+
+#create ec2 instance
+resource "aws_instance" "winserver" {
+    ami             = "${data.aws_ami.test_iis_server.id}"
+    instance_type   = "t3.micro"
+
+    tags {
+      description = "Alipui TF Test"
+    }
+}
